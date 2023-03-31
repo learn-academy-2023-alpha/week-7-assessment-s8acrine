e3a52d903aedd465bfcc97faffca0d180e7cf424
Aaron Criner <aaron.criner@me.com>
2023-03-31 15:57:06 -0700

    challenges and interview questions complete:

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,33 +14,112 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const people = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+describe("makeIntros", () => {
+  it("provides a name and occupation for each member of the people object", () => {
+    const people = [
+      { name: "ford prefect", occupation: "a hitchhiker" },
+      { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+      { name: "arthur dent", occupation: "a radio employee" },
+    ];
+    // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+
+    expect(makeIntrosButUgly(people)).toEqual([
+      "Ford Prefect is a hitchhiker.",
+      "Zaphod Beeblebrox is president of the galaxy.",
+      "Arthur Dent is a radio employee.",
+    ]);
+  });
+});
 
 // b) Create the function that makes the test pass.
+
+// Pseudocode:
+// map through the array, for each object, extract the name, capitalize it, then use string interpolation to return an array of statements
+// capitalize the name:
+//// splitting the string into an array
+//// use map to iterate on the first and last name
+//// use .toUpperCase on the first value of each array
+//// rejoing the mapped array using join(" ")
+// use string interpolation to return the introduction using dot notation to access the occupation
+
+const makeIntros = (array) => {
+  return array.map((person) => {
+    let capitalName = person.name
+      .split(" ")
+      .map((name) => {
+        return name[0].toUpperCase() + name.substring(1);
+      })
+      .join(" ");
+    return `${capitalName} is ${person.occupation}.`;
+  });
+};
+
+// declaring variables is SO last year, let's make it "simpler™️"
+
+const makeIntrosButUgly = (array) => {
+  return array.map((person) => {
+    return `${person.name
+      .split(" ")
+      .map((name) => {
+        return name[0].toUpperCase() + name.substring(1);
+      })
+      .join(" ")} is ${person.occupation}.`;
+  });
+};
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+describe("moduloThreeEvenIfTheArrayIsTrash", () => {
+  it("returns an array of only the remainder of dividing numbers by three ", () => {
+    const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false];
+    // Expected output: [ 2, 0, -1, 0 ]
+    const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true];
+    // Expected output: [ 2, 1, -1 ]
+    expect(moduloThreeEvenIfTheArrayIsTrash(hodgepodge1)).toEqual([
+      2, 0, -1, 0,
+    ]);
+    expect(moduloThreeEvenIfTheArrayIsTrash(hodgepodge2)).toEqual([2, 1, -1]);
+  });
+});
 
 // b) Create the function that makes the test pass.
+
+//PseudoCode
+// Use a combination of filter(), .map() and the modulo operator % to return only the numbers in an array, then use the modulo operator to extract the remainder when divided by three
+// use .filter(typeof value === "number") to extract only the values that are numbers
+// .map over the array and perform a modulo 3
+
+moduloThreeEvenIfTheArrayIsTrash = (array) => {
+  return array
+    .filter((value) => typeof value === "number")
+    .map((value) => value % 3);
+};
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
+describe("sumOfCubes", () => {
+  it("returns the sum of the cubes of each value in the array", () => {
+    const cubeAndSum1 = [2, 3, 4];
+    // Expected output: 99
+    const cubeAndSum2 = [0, 5, 10];
+    // Expected output: 1125
+    expect(sumOfCubes(cubeAndSum1)).toEqual(99);
+    expect(sumOfCubes(cubeAndSum2)).toEqual(1125);
+  });
+});
 
 // b) Create the function that makes the test pass.
+//Pseudocode:
+// use the reduce method to accumalate a running total of each value in the array, use the exponent method ** to cube each value as it's accumalated
+
+sumOfCubes = (array) => {
+  return array
+    .map((value) => value ** 3)
+    .reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    });
+};
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -7,24 +7,34 @@
 # FILE: app/controller/blog_posts_controller.rb
 
 # ---1)
+# Defines the class BlogPostController, which inherits from the ApplicationController superclass
 class BlogPostsController < ApplicationController
   def index
     # ---2)
+    # instantiates the instance variable posts, which is set equal to BlogPost.all (ActiveRecord pull for all instances of the BlogPost model)
+    # used in the index route to show all blog posts
     @posts = BlogPost.all
   end
 
   # ---3)
+  # instantiates the instance variable post, which is set to the BlogPost which has an id mathching the id passed in the params
+  # used to in the show route to show a single blog post
   def show
     @post = BlogPost.find(params[:id])
   end
 
   # ---4)
+  # instantiates teh instance variable post, which is set to the BlogPost.new value.
+  # using with routing to route to a form which creates a new blogpost
   def new
     @post = BlogPost.new
   end
 
   def create
     # ---5)
+    # instantiates teh instance variable post, which is set to the creation of a new instance of BlogPost with blog_post_params
+    # Creates a new instance of a blog post using the parameters entered from the parameters. 
+    # if the creation of the blog post is valid, redirects to the show route of the created post
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -33,12 +43,14 @@ class BlogPostsController < ApplicationController
 
   def edit
     # ---6)
+    # gets the form to edit a blog post determined by the id in the paramaters
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
     # ---7)
+    # uses the update method to update the values of a blog post using the parameters provided
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -49,14 +61,17 @@ class BlogPostsController < ApplicationController
     @post = BlogPost.find(params[:id])
     if @post.destroy
       # ---8)
+      # redirects to the path blog_posts from the routes file. This is likely the index page, which shows all blog posts
       redirect_to blog_posts_path
     end
   end
 
   # ---9)
+  # creates a set of methods that can ONLY be called on internally from this file. Prevents the calling of this method from anywhere other than directly in this file
   private
   def blog_post_params
     # ---10)
+    # sets the required MODEL to be edited, as well as the permitted values that can be edited
     params.require(:blog_post).permit(:title, :content)
   end
 end
